11f5a7637defb432aff7401c66dbc44e17457b66
Lena <lenatao15@gmail.com>
2026-07-10 16:33:52 -0700

    Auto-run migrations inside wsgi.py on application startup

--- FeedProject/wsgi.py
+++ FeedProject/wsgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FeedProject.settings')
 
 application = get_wsgi_application()
+
+try:
+    print("Auto-running database migrations...")
+    call_command('migrate', no_input=True)
+    print("Database migrations applied successfully.")
+except Exception as e:
+    print(f"Error auto-running migrations: {e}")
+
